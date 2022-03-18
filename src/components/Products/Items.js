803e005aac1ad1f176b16653351f81b902f7a1d5
Zoran G <68257343+verona-hub@users.noreply.github.com>
2022-03-18 03:10:04 +0100

    - Items: added products pictures and add to cart button

--- src/components/Products/Items.js
+++ src/components/Products/Items.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
 
 // Components
 import MaterialItem from '../Utility/MaterialItem';
@@ -7,14 +8,23 @@ import ItemsInfo from './ItemsInfo';
 
 
 const Items = () => {
+
+    const addToCart = () => {
+        console.log('Added to cart')
+    };
+
     return (
         <div>
-            <Grid container spacing={12} justify='center'>
+            <Grid container spacing={ 12 } justify='center'>
                 {
-                    ItemsInfo.map( item => (
-                        <Grid item key={item.id} lg={4} md={6} sm={6} xs={12}>
+                    ItemsInfo.map(item => (
+                        <Grid item key={ item.id } lg={ 6 } md={ 6 } sm={ 6 } xs={ 12 }>
+                            <div>
+                                <img src={ item.image } alt='product'/>
+                            </div>
                             <h2>{ item.name }</h2>
                             <h4>{ item.price }</h4>
+                            <Button onClick={addToCart} variant='contained' size='large'> Add to cart </Button>
                         </Grid>
                     ))
                 }
--- src/components/Products/ItemsInfo.js
+++ src/components/Products/ItemsInfo.js
@@ -1,30 +1,42 @@
+import smartHubImg from '../../img/smart-hub.jpg';
+import motionSensorImg from '../../img/motion-sensor.jpg';
+import wirelessCameraImg from '../../img/wireless-camera.jpg';
+import smokeDetectorImg from '../../img/smoke-detector.jpg';
+import waterLeakSensorImg from '../../img/water-leak-sensor.jpg';
+
 
 const ItemsInfo = [
     {
         id: 1,
         name: 'Smart Hub',
+        image: smartHubImg,
         price: 49.99
     },
     {
         id: 2,
         name: 'Motion Sensor',
+        image: motionSensorImg,
         price: 24.99
     },
     {
         id: 3,
         name: 'Wireless Camera',
+        image: wirelessCameraImg,
         price: 99.99
     },
     {
         id: 4,
         name: 'Smoke Sensor',
+        image: smokeDetectorImg,
         price: 19.99
     },
     {
         id: 5,
         name: 'Water Leak Sensor',
+        image: waterLeakSensorImg,
         price: 14.99
     }
 ];
+
 
 export default ItemsInfo;
